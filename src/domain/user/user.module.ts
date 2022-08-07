8ef8b9bb34c5f6253af76efd6f6b01d13a5711ac
Andrew <yakorshak@gmail.com>
2022-08-07 20:03:41 +0300

    refactored DTO's

--- src/domain/user/user.module.ts
+++ src/domain/user/user.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { RolesEntity } from './entities/roles.entity';
 import { UserEntity } from './entities/user.entity';
 import { UserService } from './user.service';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([UserEntity])],
+  imports: [TypeOrmModule.forFeature([UserEntity, RolesEntity])],
   providers: [UserService],
   exports: [UserService],
 })
